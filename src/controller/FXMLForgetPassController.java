e89ecf734c5db6080a0335ad0d36c6438bc189c1
Daniil Antsyferov <daniil@hunterdigital.dev>
2021-04-24 20:37:39 +0200

    added Home screen and authorization logic

--- src/controller/FXMLForgetPassController.java
+++ src/controller/FXMLForgetPassController.java
@@ -5,11 +5,20 @@
  */
 package controller;
 
+import DBAccess.Connect4DAOException;
+import application.ApplicationState;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import model.Connect4;
+import util.Navigation;
 
 /**
  * FXML Controller class
@@ -20,13 +29,54 @@ public class FXMLForgetPassController implements Initializable {
 
     @FXML
     private Button BSendPass;
-
+    
+    @FXML
+    private Text TError;
+    
+    @FXML
+    private TextField TFUsername;
+    
+    private Connect4 db;
+    private ApplicationState applicationState;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        initDB();
+        initAppState();
     }    
+    
+    @FXML
+    private void recoverPassword() {
+        if(db.exitsNickName(TFUsername.getText())) {
+            TError.setVisible(false);
+            showInfoDialog();
+            applicationState.setForgotPassNickName(TFUsername.getText());
+            Navigation.navigateToConfirmCode((Stage) BSendPass.getScene().getWindow(), getClass());
+        } else {
+            TError.setVisible(true);
+        }
+    }
+    
+    private void initDB() {
+        try {
+            db = Connect4.getSingletonConnect4();
+        } catch (Connect4DAOException err) {
+            System.out.println(err);
+        }
+    }
+    
+    private void initAppState() {
+        applicationState = ApplicationState.getInstance();
+    }
+    
+    private void showInfoDialog() {
+        Alert dialog = new Alert(AlertType.INFORMATION);
+        dialog.setTitle("Email");
+        dialog.setHeaderText("Correo electrónico ha sido mandado");
+        dialog.setContentText("Tu código es 111");
+        dialog.showAndWait();
+    }
     
 }
