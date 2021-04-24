7f2b2ef84c27c2b40564dd2e8b0d210eb6acea0f
Daniil Antsyferov <daniil@hunterdigital.dev>
2021-04-24 20:27:56 +0200

    added Home screen and authorization logic

---
+++ src/controller/FXMLCodeConfirmController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import util.Navigation;
+
+/**
+ *
+ * @author 44puk
+ */
+public class FXMLCodeConfirmController implements Initializable {
+    
+    @FXML
+    private TextField TFCode;
+    
+    @FXML
+    private Text TError;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+    
+    @FXML
+    private void validateCode(){
+        if(TFCode.getText().equals("111")) {
+             Navigation.navigateToResetPass((Stage) TFCode.getScene().getWindow(), getClass());
+        } else {
+            TError.setVisible(true);
+        }
+    }
+    
+}
