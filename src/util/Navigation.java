839dfd89b2401eb2f60ac1e99499d0c6fa70d6a6
Daniil Antsyferov <daniil@hunterdigital.dev>
2021-04-24 20:38:12 +0200

    added Home screen and authorization logic

---
+++ src/util/Navigation.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 44puk
+ */
+public class Navigation {
+    public static void navigateToSignIn(Stage stage, Class classType){
+        navigate(stage, classType, "/view/FXMLSignIn.fxml");
+    }
+    
+    public static void navigateToForgotPass(Stage stage, Class classType){
+        navigate(stage, classType, "/view/FXMLForgetPass.fxml");
+    }
+    
+    public static void navigateToHome(Stage stage, Class classType){
+        navigate(stage, classType, "/view/FXMLHome.fxml");
+    }
+    
+    public static void navigateToResetPass(Stage stage, Class classType){
+        navigate(stage, classType, "/view/FXMLResetPass.fxml");
+    }
+    
+    public static void navigateToIni(Stage stage, Class classType){
+        navigate(stage, classType, "/view/FXMLIni.fxml");
+    }
+    
+    public static void navigateToConfirmCode(Stage stage, Class classType){
+        navigate(stage, classType, "/view/FXMLCodeConfirm.fxml");
+    }
+    
+    
+    private static void navigate(Stage stage, Class classType, String res) {
+        try {
+            Parent root = FXMLLoader.load(classType.getResource(res));
+          
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch(IOException err) {
+            System.out.println(err);
+        }
+    }
+}
