ff88602776b7587fddb588526e48f1a3888f5aaa
Daniil Antsyferov <daniil@hunterdigital.dev>
2021-04-24 20:37:45 +0200

    added Home screen and authorization logic

---
+++ src/controller/FXMLHomeController.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import application.ApplicationState;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import model.Player;
+import util.Navigation;
+
+/**
+ *
+ * @author 44puk
+ */
+public class FXMLHomeController implements Initializable{
+    
+    @FXML
+    private ImageView IVFirstPlayerAvatar;
+    
+    @FXML
+    private Text TFirstPlayerName;
+    
+    @FXML
+    private ImageView IVSecondPlayerAvatar;
+    
+    @FXML
+    private Text TSecondPlayerName;
+    
+    @FXML
+    private Button BSecondPlayerButton;
+    
+    
+    
+    private ApplicationState applicationState;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initAppState();
+        initFirstPlayer();
+        initSecondPlayer();
+    } 
+    
+    @FXML
+    private void firstPlayerLogOut() {
+        if(applicationState.logOutFirstPlayer()) {
+            // another player is still logged in so he is player 1 now
+            initFirstPlayer();
+            initSecondPlayer();
+        } else {
+            // no more logged in player, go to log in screen
+            Navigation.navigateToSignIn((Stage) IVFirstPlayerAvatar.getScene().getWindow(), getClass());
+        }
+    }
+    
+    @FXML
+    private void secondPlayerAction() {
+        Player secondPlayer = applicationState.getSecondPlayer();
+        if(secondPlayer != null) {
+            applicationState.logOutSecondPlayer();
+            initSecondPlayer();
+        } else {
+            Navigation.navigateToSignIn((Stage) IVFirstPlayerAvatar.getScene().getWindow(), getClass());
+        }
+    }
+   
+    private void initSecondPlayer() {
+        
+        Player secondPlayer = applicationState.getSecondPlayer();
+        if(secondPlayer != null) {
+            IVSecondPlayerAvatar.setVisible(true);
+            IVSecondPlayerAvatar.setImage(secondPlayer.getAvatar());
+            TSecondPlayerName.setText(secondPlayer.getNickName());
+            BSecondPlayerButton.setText("Salir");
+        } else {
+            IVSecondPlayerAvatar.setVisible(false);
+            TSecondPlayerName.setText("Jugador 2");
+            BSecondPlayerButton.setText("Iniciar sesión");
+        }
+        
+    }
+    
+    private void initFirstPlayer() {
+        Player firstPlayer = applicationState.getFirstPlayer();
+        IVFirstPlayerAvatar.setImage(firstPlayer.getAvatar());
+        TFirstPlayerName.setText(firstPlayer.getNickName());
+    }    
+    private void initAppState() {
+        applicationState = ApplicationState.getInstance();
+    }
+    
+}
