cfafbd29ab883283feda458c869e7f6be70e4424
Daniil Antsyferov <daniil@hunterdigital.dev>
2021-04-24 20:38:01 +0200

    added Home screen and authorization logic

--- src/controller/FXMLResetPassController.java
+++ src/controller/FXMLResetPassController.java
@@ -5,9 +5,17 @@
  */
 package controller;
 
+import DBAccess.Connect4DAOException;
+import application.ApplicationState;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import model.Connect4;
+import model.Player;
+import util.Navigation;
 
 /**
  * FXML Controller class
@@ -16,12 +24,43 @@ import javafx.fxml.Initializable;
  */
 public class FXMLResetPassController implements Initializable {
 
+    
+    @FXML
+    private Text TPass;
+    
+    private Connect4 db;
+    private ApplicationState applicationState;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        initDB();
+        initAppState();
+        setText();
     }    
+    
+    @FXML
+    private void navigateToIni() {
+        Navigation.navigateToIni((Stage) TPass.getScene().getWindow(), getClass());
+    }
+    
+    private void setText() {
+        Player p = db.getPlayer(applicationState.getForgotPassNickName());
+        TPass.setText(p.getPassword());
+    }
+    
+    private void initDB() {
+        try {
+            db = Connect4.getSingletonConnect4();
+        } catch (Connect4DAOException err) {
+            System.out.println(err);
+        }
+    }
+    
+    private void initAppState() {
+        applicationState = ApplicationState.getInstance();
+    }
     
 }
